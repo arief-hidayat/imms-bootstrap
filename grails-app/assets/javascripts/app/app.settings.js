2885126cf5e7627e7e704095a90474e6cb11c0b1
ärief <mr.arief.hidayat@gmail.com>
2014-05-26 17:07:32 +0800

    Api for server side table is ready

--- grails-app/assets/javascripts/app/app.settings.js
+++ grails-app/assets/javascripts/app/app.settings.js
@@ -2,3 +2,20 @@ App = window.App || {};
 App.options = App.options || {
     language : "en"
 };
+
+App.dt = App.dt || { };
+App.dt.config = App.dt.config || {};
+
+App.dt.config.table = {
+    asset : {
+        columns : [ { "data": "code" }, { "data": "name" }, { "data": "assetType" }, { "data": "status" }, { "data": "locationCd" }],
+        order : [[1, 'asc']]
+    }
+};
+
+App.dt.config.customUrl = {
+//        asset : {
+//            url : "only for custom",
+//            extraParams : function(request) { }
+//        }
+};
--- grails-app/assets/javascripts/imms.datatable.pack.js
+++ grails-app/assets/javascripts/imms.datatable.pack.js
@@ -8,6 +8,57 @@
 //= require app/app.settings
 //= require jquery
 //= require jquery.dataTables.min
+//= require dt/dataTables.ext
 //= require dataTables.bootstrap
+//= require imms.backbone.pack
 //= require_self
 
+(function($, Backbone, App){
+    App.dataTableOptions = function(key) { // key is domainName e.g. asset, but it might be customized i.e. workOrder/closed
+        var tableConf = App.dt.config.table[key];
+        var customUrlConf = App.dt.config.customUrl[key];
+
+        var pipelineOpt = { url : "dataTable/" + key};
+        if(customUrlConf != undefined) {
+            if(customUrlConf.url != undefined) pipelineOpt.url = customUrlConf.url;
+            if(customUrlConf.data != undefined) pipelineOpt.data = customUrlConf.data;
+        }
+        var ret = {
+            "processing": true,
+            "serverSide": true,
+            "columns" : tableConf.columns,
+//            language: {
+//                url: "/assets/localization/table/"+ App.options.language + ".json"
+//            },
+            "rowCallback": function( row, data, displayIndex ) { // make row selectable
+                var selected = $('#example').data('selectedRows');
+                if ( $.inArray(data.DT_RowId, selected) !== -1 ) { //must have row_id
+                    $(row).addClass('selected');
+                }
+            },
+            "ajax": $.fn.dataTable.pipeline(pipelineOpt)
+        };
+        if(tableConf.order != undefined) { ret.order = tableConf.order; }
+        return ret;
+    };
+
+    App.initTable = function($root, key) {
+        $root.data('selectedRows', []);
+        $root.dataTable( App.dataTableOptions(key));
+        $root.find('tbody').on('click', 'tr', function () {
+            var id = this.id;
+            var selected = $root.data('selectedRows');
+            var index = $.inArray(id, selected);
+            if ( index === -1 ) {
+                selected.push( id );
+            } else {
+                selected.splice( index, 1 );
+            }
+
+            $(this).toggleClass('selected');
+            $root.data('selectedRows', selected);
+        } );
+    };
+
+
+})(jQuery, Backbone, App);
